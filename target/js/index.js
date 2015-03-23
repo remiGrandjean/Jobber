400d352535b360e20f7181bdbf544815a04ce46b
Remi Grandjean <remi1360@voila.fr>
2015-03-23 11:39:32 +0100

    slide

--- target/js/index.js
+++ target/js/index.js
@@ -1,28 +1,67 @@
-$(document).ready(function(){
+$(document).ready(function(event) {
+	
+	$("div#swipe_like").on( "click", function() {
+		swipeLike();
+	});	
 
-    $(".buddy").on("swiperight",function(){
-      $(this).addClass('rotate-left').delay(700).fadeOut(1);
-      $('.buddy').find('.status').remove();
+	$("div#swipe_dislike").on( "click", function() {
+		swipeDislike();
+	});	
 
-      $(this).append('<div class="status like">Like!</div>');      
-      if ( $(this).is(':last-child') ) {
-        $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-       } else {
-          $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-       }
-    });  
+	addNewProfile();
 
-   $(".buddy").on("swipeleft",function(){
-    $(this).addClass('rotate-right').delay(700).fadeOut(1);
-    $('.buddy').find('.status').remove();
-    $(this).append('<div class="status dislike">Dislike!</div>');
+	function swipe() {
+		Draggable.create("#photo", {
+		   	throwProps:true,
+		   	onDragEnd:function(endX) {
+	   			if(Math.round(this.endX) > 0 ) {
+	   				swipeLike();			
+	   			}
+	   			else {
+	   				swipeDislike();
+	   			}
+	   			console.log(Math.round(this.endX));
+			}
+		});
+	}
 
-    if ( $(this).is(':last-child') ) {
-     $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-      alert('Na-na!');
-     } else {
-        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-    } 
-  });
+	function swipeLike() {
+		
+			var $photo = $("div.content").find('#photo');
+
+			var swipe = new TimelineMax({repeat:0, yoyo:false, repeatDelay:0, onComplete:remove, onCompleteParams:[$photo]});
+			swipe.staggerTo($photo, 0.8, {bezier:[{left:"+=400", top:"+=300", rotation:"60"}], ease:Power1.easeInOut});
+
+			addNewProfile();
+	}
+
+	function swipeDislike() {
+		
+			var $photo = $("div.content").find('#photo');
+
+			var swipe = new TimelineMax({repeat:0, yoyo:false, repeatDelay:0, onComplete:remove, onCompleteParams:[$photo]});
+			swipe.staggerTo($photo, 0.8, {bezier:[{left:"+=-350", top:"+=300", rotation:"-60"}], ease:Power1.easeInOut});
+
+			addNewProfile();
+	}
+
+	function remove(photo) {
+	    $(photo).remove();
+	}
+
+	function addNewProfile() {
+		var names = ['Lieke', 'Christina', 'Sanne', 'Soraya', 'Chanella', 'Larissa', 'Michelle'][Math.floor(Math.random() * 7)];
+		var ages = ['19','22','18','27','21', '18', '24'][Math.floor(Math.random() * 7)]
+		var photos = ['1', '2', '3', '4', '5', '6', '7'][Math.floor(Math.random() * 7)]
+		$("div.content").prepend('<div class="photo" id="photo" style="background-image:url(http://web.arjentienkamp.com/codepen/tinder/photo'+photos+'.jpg)">'
+    	+ '<span class="meta">' 
+    	+ '<p>'+names+', '+ages+'</p>' 
+    	+ '<span class="moments">0</span>' 
+    	+ '<span class="users">0</span>' 
+    	+ '</span>' 
+    	+ '</div>');
+
+    	swipe();
+	}
 
 });
