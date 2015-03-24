ce99d4066bef76d67e5c15fc5e0204febcbeff32
Remi Grandjean <remi1360@voila.fr>
2015-03-24 15:06:43 +0100

    serveur marche

--- src/bdd/GenericDao.java
+++ src/bdd/GenericDao.java
@@ -1,6 +1,5 @@
 package bdd;
 
-
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
@@ -13,7 +12,7 @@ import classeJava.User;
 public interface GenericDao {
 
 	// User---------------------------------------------------------------
-	@SqlUpdate("create table if not exists Users (email varchar(10) primary key,mdp var(10),nom varchar(15),prenom varchar(15),age integer, region text,role text)")
+	@SqlUpdate("create table if not exists Users (email varchar(10) primary key,mdp var(10),nom varchar(15),prenom varchar(15),age integer, region text,role text,formation text,experience text,loisirs text,information text)")
 	void createUsersTable();
 
 	@SqlUpdate("insert into Users (email,mdp,nom,prenom,age,region,role) values (:email,:mdp,:nom,:prenom,:age,:region)")
@@ -30,6 +29,25 @@ public interface GenericDao {
 	@SqlQuery("select * from Users where email = :email and mdp = :mdp")
 	@RegisterMapperFactory(BeanMapperFactory.class)
 	User findUsersByLog(@Bind("email") String mail, @Bind("mdp") String mdp);
+
+	@SqlUpdate("insert into Users (formation) values (:formation)")
+	@GetGeneratedKeys
+	int addFormation(@Bind("formation") String formation,
+			@Bind("email") String mail);
+
+	@SqlUpdate("insert into Users (experience) values (:experience) where email= :email")
+	@GetGeneratedKeys
+	int addExperience(@Bind("experience") String experience,
+			@Bind("email") String mail);
+
+	@SqlUpdate("insert into Users (loisirs) values (:loisirs) where email= :email")
+	@GetGeneratedKeys
+	int addLoisirs(@Bind("loisirs") String loisirs, @Bind("email") String mail);
+
+	@SqlUpdate("insert into Users (information) values (:information) where email= :email")
+	@GetGeneratedKeys
+	int addInformation(@Bind("information") String information,
+			@Bind("email") String mail);
 
 	@SqlUpdate("drop table if exists Users")
 	void dropUserTable();
