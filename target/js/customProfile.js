ef2f468ff83ba12f7d99026198706b2cd9b6e66e
Adhemar Wissocq <adhemar.wissocq@gmail.com>
2015-03-24 16:08:22 +0100

    eclatement script en signUpForm.js & customProfile.js ++++ Proprete code sur l'indentation

---
+++ target/js/customProfile.js
@@ -0,0 +1,78 @@
+function loadProfil(){
+	var nom,prenom,region,age,formation,experience,loisirs,information;
+	$.ajax({
+	    type: "GET",
+        url: "http://localhost:9876/v1/jobber/user",			
+	    contentType: "application/json; charset=utf-8",				   
+		data: '{"nom": "'+nom+'","prenom":"'+prenom+'","region":"'+region+'","age":"'+age+'","formation":"'+formation+'","experience":"'+experience+'","loisirs":"'+loisirs+'","information":"'+information+'"}',
+	    dataType: "json",
+		success:function(){
+        	alert(data);
+        },
+        error:function(){
+	    	alert("Error");
+		}
+	});
+	$('inforperso').replaceWith(nom+' '+prenom+' '+age+'</br>'+region);
+	$('myFormation').replaceWith(formation);
+	$('myExperience').replaceWith(experience);
+	$('myLoisirs').replaceWith(loisirs);
+	$('myInformation').replaceWith(information);
+}
+
+function addFormation(){
+	$('#myFormation').append("<input type='text' id='inputFormation' value='' placeholder='Ajouter une formation'><input type='month' id='dateformation' placeholder='MM/AAAA'><input type='button' id='valideformation' value='Valider'>");
+	$('#valideformation').click(function(){
+		if($('#inputFormation').val()!==undefined && $('#dateformation').val()!==undefined){
+			var text = $('#inputFormation').val();
+			var date = $('#dateformation').val();
+			$('#inputFormation').detach();
+			$('#dateformation').detach();
+			$('#valideformation').detach();
+			if(text!=='' && date!==''){
+				$('#myFormation').prepend('<b>'+text+'  '+date+'</b></br>');
+			}
+		}
+	});
+}
+function addExperience(){
+	$('#myExperience').append("<input type='text' id='inputExperience' value='' placeholder='Ajouter une experience'><input type='month' id='dateexperience' placeholder='MM/AAAA'><input type='button' id='valideexperience' value='Valider'>");
+	$('#valideexperience').click(function(){
+		if($('#inputExperience').val()!==undefined && $('#dateexperience').val()!==undefined ){
+			var text = $('#inputExperience').val();
+			var date = $('#dateexperience').val();
+			$('#inputExperience').detach();
+			$('#dateexperience').detach();
+			$('#valideexperience').detach();
+			if(text!=='' && date!==''){
+				$('#myExperience').prepend('<b>'+text+'  '+date+'</b></br>');
+			}
+		}
+	});
+}
+function addLoisirs(){
+	$('#myLoisirs').append("<textarea rows='3' cols='50' id='arealoisirs' placeholder='Loisirs...''></textarea><input type='button' id='valideloisirs' value='Valider'>");
+	$('#valideloisirs').click(function(){
+		if($('#arealoisirs').val()!==undefined ){
+			var text = $('#arealoisirs').val();
+			$('#arealoisirs').detach();
+			$('#valideloisirs').detach();
+			if(text!=='' ){
+				$('#myLoisirs').prepend('<b>'+text+'</b></br>');
+			}
+		}
+	});
+}
+function addInformation(){
+	$('#myInformation').append("<textarea rows='3' cols='50' id='areainformation' placeholder='Informations complémentaires...''></textarea><input type='button' id='valideinformation' value='Valider'>");
+	$('#valideinformation').click(function(){
+		if($('#areainformation').val()!==undefined ){
+			var text = $('#areainformation').val();
+			$('#areainformation').detach();
+			$('#valideinformation').detach();
+			if(text!=='' ){
+				$('#myInformation').prepend('<b>'+text+'</b></br>');
+			}
+		} 	
+	});
+}
---
+++ target/js/singUpForm.js
@@ -0,0 +1,111 @@
+function valid_form(){
+	if(document.myform.name.value == ""){
+		alert("Veuillez entrer votre nom"); 
+		document.myform.name.focus();
+		return false;
+	}
+	if(document.myform.surname.value == ""){
+		alert("Veuillez entrer votre prénom");
+		document.myform.surname.focus();
+		return false;
+	} 
+	if(document.myform.age.value < 0){
+		alert("Veuillez entrer un âge valide");
+		document.myform.surname.focus();
+		return false;
+	}
+	if(document.myform.mail.value !== document.myform.confirm_mail.value){
+		alert("Erreur dans la répétition du mail");
+		document.myform.mail.focus();
+		return false;
+	}
+	if(document.myform.passwd.value !== document.myform.confirm_passwd.value){
+		alert("Erreur dans la répétition du mot de passe");
+		document.myform.passwd.focus();
+		return false;
+	}
+				
+};
+$('connectionform').on('submit',function(e){
+	e.preventDefault();
+	var email=$('login').val();
+	var mdp =$('password').val();
+	a = new authen(email,mdp);
+	console.log(a);
+	$.ajax({
+	type: "GET",
+	url: "http://localhost:9876/v1/jobber/user",			
+	contentType: "application/json; charset=utf-8",				   
+	data: '{"email": "' + email + '", "mdp" : "' + mdp + '"}',
+	dataType: "json", 
+	success:function(){
+	alert(data);
+	},
+	error:function(){
+	alert("Error");
+	}
+});
+
+
+
+			});
+			$('myform').on('submit',function(e){
+					console.log("boid");
+					e.preventDefault();
+					var $this=$(this);
+					var nom =$('nom').val();
+					var prenom =$('prenom').val();
+					var mail =$('mail').val();
+					var role =$('role').val();
+					var mdp =$('passwd').val();
+					var region =$('region').val();
+
+					var data= {
+						'email': mail,
+						'mdp': mdp,
+						'nom': nom,
+						'prenom': prenom,
+						'age': age,
+						'region': region,
+						'role': role,
+
+					}
+					console.log(email+mdp+nom+prenom+age+region+role);
+
+
+					 $.ajax({
+				        type: "POST",
+				        url: "http://localhost:9876/v1/jobber/addUser",			
+				        contentType: "application/json; charset=utf-8",				   
+				        data: JSON.stringify(data),
+				        dataType: "json",
+				        success:function(){
+				        	alert("gg");
+				        },
+				        error:function(){
+				        	alert("pas gg");
+				        }
+				    });
+				});
+			function soumettre(id) {
+			doucment.getElementById(id).target = 'formulaire';
+			window.open('','formulaire',width='800', height='600');
+		}
+			$(document).ready(function(){
+				$('.slider_wrapper')._TMS({
+					show:0,
+					pauseOnHover:false,
+					prevBu:'.prev',
+					nextBu:'.next',
+					playBu:false,
+					duration:800,
+					preset:'fade',
+					pagination:true,//'.pagination',true,'<ul></ul>'
+					pagNums:false,
+					slideshow:1000,
+					numStatus:false,
+					banners: 'fade',
+					waitBannerAnimation:false,
+					progressBar:false
+				});
+			});
